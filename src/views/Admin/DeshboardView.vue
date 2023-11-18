baf2a4bc38041b0c6a98f573f011f067c260c640
Hazrat02 <hazratbd80@gmail.com>
2023-11-18 09:31:17 -0800

    Admin Dashboard Change

--- src/views/Admin/DeshboardView.vue
+++ src/views/Admin/DeshboardView.vue
@@ -10,19 +10,19 @@
         <div class="container-fluid">
           <div class="row gy-4">
             <div class="col-md-3 col-sm-6">
-              <div class="card mb-0">
+              <RouterLink to="/admin/user" class="card mb-0">
                 <div class="card-body">
                   <div
                     class="d-flex align-items-end justify-content-between mb-2"
                   >
-                    <div class="me-2">
+                    <div to="/admin/user" class="me-2">
                       <svg
                         class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2"
                       >
                         <use xlink:href="#user-1"></use>
                       </svg>
                       <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">
-                        Total User
+                        Total Users..
                       </p>
                     </div>
                     <p class="text-xxl lh-1 mb-0 text-dash-color-1">27</p>
@@ -38,10 +38,10 @@
                     ></div>
                   </div>
                 </div>
-              </div>
+              </RouterLink>
             </div>
             <div class="col-md-3 col-sm-6">
-              <div class="card mb-0">
+              <RouterLink to="/admin/transaction" class="card mb-0">
                 <div class="card-body">
                   <div
                     class="d-flex align-items-end justify-content-between mb-2"
@@ -53,7 +53,7 @@
                         <use xlink:href="#stack-1"></use>
                       </svg>
                       <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">
-                        New Projects
+                        Total Deposit
                       </p>
                     </div>
                     <p class="text-xxl lh-1 mb-0 text-dash-color-2">375</p>
@@ -69,41 +69,10 @@
                     ></div>
                   </div>
                 </div>
-              </div>
+              </RouterLink>
             </div>
             <div class="col-md-3 col-sm-6">
-              <div class="card mb-0">
-                <div class="card-body">
-                  <div
-                    class="d-flex align-items-end justify-content-between mb-2"
-                  >
-                    <div class="me-2">
-                      <svg
-                        class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2"
-                      >
-                        <use xlink:href="#survey-1"></use>
-                      </svg>
-                      <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">
-                        New Invoices
-                      </p>
-                    </div>
-                    <p class="text-xxl lh-1 mb-0 text-dash-color-3">140</p>
-                  </div>
-                  <div class="progress" style="height: 3px">
-                    <div
-                      class="progress-bar bg-dash-color-3"
-                      role="progressbar"
-                      style="width: 55%"
-                      aria-valuenow="55"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-              <div class="card mb-0">
+              <RouterLink to="/admin/transaction" class="card mb-0">
                 <div class="card-body">
                   <div
                     class="d-flex align-items-end justify-content-between mb-2"
@@ -115,7 +84,7 @@
                         <use xlink:href="#paper-stack-1"></use>
                       </svg>
                       <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">
-                        All Projects
+                        Total Withdraw
                       </p>
                     </div>
                     <p class="text-xxl lh-1 mb-0 text-dash-color-4">41</p>
@@ -131,8 +100,40 @@
                     ></div>
                   </div>
                 </div>
-              </div>
+              </RouterLink>
             </div>
+            <div class="col-md-3 col-sm-6">
+              <RouterLink to="/admin/offer" class="card mb-0">
+                <div class="card-body">
+                  <div
+                    class="d-flex align-items-end justify-content-between mb-2"
+                  >
+                    <div class="me-2">
+                      <svg
+                        class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2"
+                      >
+                        <use xlink:href="#survey-1"></use>
+                      </svg>
+                      <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">
+                        Total Offers..
+                      </p>
+                    </div>
+                    <p class="text-xxl lh-1 mb-0 text-dash-color-3">14</p>
+                  </div>
+                  <div class="progress" style="height: 3px">
+                    <div
+                      class="progress-bar bg-dash-color-3"
+                      role="progressbar"
+                      style="width: 55%"
+                      aria-valuenow="55"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              </RouterLink>
+            </div>
+            
           </div>
         </div>
       </section>
@@ -145,6 +146,8 @@
 <script>
 // import { useAuthUserStore } from "../../store/user";
 // import { transactionStore } from "../../store/transaction";
+
+import { RouterLink } from 'vue-router';
 
 // export default {
 //   data() {
